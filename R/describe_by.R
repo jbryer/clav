d390646e703542b22d60b6f5eb5d5a4e8e6920cd
Jason Bryer <jason@bryer.org>
2025-06-12 09:35:29 -0400

    Fixed import.

--- R/describe_by.R
+++ R/describe_by.R
@@ -1,12 +1,3 @@
-#' Calculate the standard error of the mean
-#'
-#' @param x a numeric vector.
-#' @param ... other parameters passed to `sd`.
-#' @return the standard error.
-se <- function(x, ...) {
-	sd(x, ...) / sqrt(length(x))
-}
-
 #' Basic summary statistics by group
 #'
 #' Calculate basic summary statistics by a grouping variable. This function is inspired by the
@@ -22,7 +13,7 @@ se <- function(x, ...) {
 #'         `variable` corresponding to the names of the data frame. Additional columns for each
 #'         function in the `funs` parameter.
 #' @export
-#' @importFrom dplyr mutate group_by summarise select
+#' @importFrom dplyr mutate group_by summarise select all_of across
 #' @importFrom reshape2 melt dcast
 #' @importFrom tidyr separate_wider_delim
 #' @examples
@@ -35,26 +26,30 @@ describe_by <- function(
 		funs = list(n = length,
 					mean = mean,
 					sd = sd,
-					se = se,
+					se = function(x, ...) { sd(x, ...) / sqrt(length(x)) },
 					median = median,
 					min = min,
 					max = max,
-					range = function(x) { diff(range(x)) })
+					range = function(x, ...) { diff(range(x, ...)) })
 ) {
 	if(nrow(df) != length(group)) {
 		stop('Number of rows in the data frame must be the same length as the grouping variable.')
 	}
 	rename <- 'group'
 	names(rename) <- group_name
+	var_sep <- '_'
+	while(length(grep(var_sep, names(df))) > 0) {
+		var_sep <- paste0(var_sep, '_')
+	}
 	df |>
 		dplyr::mutate(group = group) |>
 		dplyr::group_by(group) |>
-		dplyr::summarise(across(names(df), .fns = funs, .names = "{.col}___{.fn}")) |>
+		dplyr::summarise(dplyr::across(names(df), .fns = funs, .names = paste0("{.col}", var_sep, "{.fn}"))) |>
 		reshape2::melt(id.vars = 'group') |>
 		tidyr::separate_wider_delim(cols = variable,
-									delim = '___',
+									delim = var_sep,
 									names = c('variable', 'fun')) |>
 		reshape2::dcast(group + variable ~ fun, value.var = 'value') |>
-		dplyr::rename(all_of(rename)) |>
-		dplyr::select(c(group_name, 'variable', names(funs)))
+		dplyr::rename(dplyr::all_of(rename)) |>
+		dplyr::select(dplyr::all_of(c(group_name, 'variable', names(funs))))
 }
